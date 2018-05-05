de3d435035f7dee1515f1b00de04225d87707e24
ashon <ashon8813@gmail.com>
2018-05-05 14:33:18 +0900

    Use page function to generate page object

--- projects/newbies-layout/src/Routes.js
+++ projects/newbies-layout/src/Routes.js
@@ -3,19 +3,31 @@ import { Link, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import A from './pages/A';
 
+const Page = (path, linkText, component) => ({
+  path, linkText, component
+});
+
+const pages = [
+  Page('/', 'Home', Home),
+  Page('/a', 'A', A)
+];
+
 const Navigator = () => (
   <nav>
     <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/a">A</Link></li>
+      {pages.map((page, idx) => <li key={idx}>
+        <Link key={idx} to={page.path}>{page.linkText}</Link>
+      </li>)}
     </ul>
   </nav>
 )
 
 const MainView = () => (
   <Switch>
-    <Route exact path='/' component={Home} />
-    <Route path='/a' component={A} />
+    {pages.map((page, idx) => <Route key={idx}
+      exact={idx === 0 ? true : false}
+      path={page.path} component={page.component}
+    />)}
   </Switch>
 )
 
